17ba505d52b3f9a733fbbc22b8934714a0fd59e7
Manolo Martin <martinmano38@gmail.com>
2021-06-15 22:33:43 -0400

    Finished about.html and about.css, updated styles.css, updated dropdown through javascript

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,17 +1,47 @@
 let dropdown = document.getElementById('dropdown');
-
+let xmark = document.getElementById('x-mark');
 let main = document.querySelector('main');
+let nav = document.getElementById('dropdown-navigation');
 
 dropdown.onclick = function() {
-    let nav = document.getElementById('dropdown-navigation');
-    
-    if(nav.style.display == 'flex'){
-        nav.style.display = 'none';
-    }else{
-        nav.style.display ='flex';
-    }
+    nav.style.display ='flex';
+    dropdown.style.display ='none';
+    xmark.style.display = 'flex';
+    xmark.style.color = 'white';
+}
+
+xmark.onclick = function() {
+    nav.style.display = 'none';
+    dropdown.style.display ='flex';
+    xmark.style.display = 'none';
+}
+
+xmark.onmouseover = function(){
+    xmark.style.backgroundColor = 'white';
+    xmark.style.color = 'black';
+    xmark.style.cursor = 'pointer'
+}
+
+xmark.onmouseleave = function(){
+    xmark.style.backgroundColor = 'black';
+    xmark.style.color = 'white';
+}
+
+dropdown.onmouseover = function(){
+    dropdown.style.backgroundColor = 'white';
+    dropdown.style.color = 'black';
+    dropdown.style.cursor = 'pointer'
+}
+
+dropdown.onmouseleave = function(){
+    dropdown.style.backgroundColor = 'black';
+    dropdown.style.color = 'white';
 }
 
 main.onclick = function() {
-    document.getElementById('dropdown-navigation').style.display = 'none';
+    nav.style.display = 'none';
+    xmark.style.display = 'none';
+    dropdown.style.display = 'flex';
+    dropdown.style.backgroundColor = 'black';
+    dropdown.style.color = 'white';
 }
